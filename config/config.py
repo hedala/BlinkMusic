41da778e1e1dfa29248201c927a078dd1c192a19
Heda <103752316+Astroheda@users.noreply.github.com>
2023-04-22 16:08:09 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -79,41 +79,41 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/2beee19e19ce208156a74.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://te.legra.ph/file/159d3f9e2d57dd02db970.jpg",
+    "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png",
 )
 
 PLAYLIST_IMG_URL = getenv(
     "PLAYLIST_IMG_URL",
-    "https://te.legra.ph/file/2beee19e19ce208156a74.jpg"
+    "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png"
 )
 
 GLOBAL_IMG_URL = getenv(
     "GLOBAL_IMG_URL",
-    "https://te.legra.ph/file/2beee19e19ce208156a74.jpg"
+    "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png"
 )
 
 STATS_IMG_URL = getenv(
     "STATS_IMG_URL",
-    "https://te.legra.ph/file/2beee19e19ce208156a74.jpg"
+    "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png"
 )
 
 TELEGRAM_AUDIO_URL = getenv(
     "TELEGRAM_AUDIO_URL",
-    "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+    "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png"
 )
 
 TELEGRAM_VIDEO_URL = getenv(
     "TELEGRAM_VIDEO_URL",
-    "https://te.legra.ph/file/2beee19e19ce208156a74.jpg"
+    "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png"
 )
 
 STREAM_IMG_URL = getenv(
     "STREAM_IMG_URL",
-    "https://te.legra.ph/file/2beee19e19ce208156a74.jpg"
+    "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png"
 )
 
 SOUNCLOUD_IMG_URL = getenv(
@@ -123,7 +123,7 @@ SOUNCLOUD_IMG_URL = getenv(
 
 YOUTUBE_IMG_URL = getenv(
     "YOUTUBE_IMG_URL",
-    "https://te.legra.ph/file/2beee19e19ce208156a74.jpg"
+    "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png"
 )
 
 SPOTIFY_ARTIST_IMG_URL = getenv(
@@ -166,9 +166,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://telegra.ph/file/56d1760224589ee370186.jpg"
+            PING_IMG_URL = "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/2beee19e19ce208156a74.jpg"
+            START_IMG_URL = "https://graph.org/file/475ca5fcdb73b6cfcb5f7.png"
