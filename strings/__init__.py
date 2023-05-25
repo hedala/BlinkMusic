af39b6fd77e7889c8892656c8ced47a483005167
Heda <astroheda@gmail.com>
2023-05-25 20:02:01 +0300

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -24,17 +24,17 @@ for filename in os.listdir(r"./strings"):
 
 
 for filename in os.listdir(r"./strings/langs/"):
-    if "tr" not in languages:
+    if "en" not in languages:
         languages["tr"] = yaml.safe_load(
-            open(r"./strings/langs/tr.yml", encoding="utf8")
+            open(r"./strings/langs/en.yml", encoding="utf8")
         )
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "tr":
+        if language_name == "en":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["tr"]:
+        for item in languages["en"]:
             if item not in languages[language_name]:
-                languages[language_name][item] = languages["tr"][item]
+                languages[language_name][item] = languages["en"][item]
