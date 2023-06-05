a41de81b9316b6670b067b4c4a4f1be40b9bb7df
Heda <astroheda@gmail.com>
2023-06-05 14:20:50 +0300

    Update id.py

--- BlinkMusic/plugins/modules/id.py
+++ BlinkMusic/plugins/modules/id.py
@@ -1,15 +1,19 @@
 from BlinkMusic import app
 from pyrogram import filters
 
-
-@app.on_message(filters.command("id"))
 def ids(_, message):
     reply = message.reply_to_message
+    total_messages = app.get_chat_members_count(message.chat.id)
     if reply:
-        message.reply_text(
-            f"**Bu senin kimliğin**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: `{reply.from_user.id}`\n**Bu grubun kimliği**: `{message.chat.id}`"
+        return (
+            f"**Bu senin kimliğin**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: `{reply.from_user.id}`\n**Bu grubun kimliği**: `{message.chat.id}`\n**Toplam gönderdiğin mesaj sayısı**: `{total_messages}`"
         )
     else:
-        message.reply(
-            f"**Bu senin kimliğin**: `{message.from_user.id}`\n**Bu grubun kimliği**: `{message.chat.id}`"
+        return (
+            f"**Bu senin kimliğin**: `{message.from_user.id}`\n**Bu grubun kimliği**: `{message.chat.id}`\n**Toplam gönderdiğin mesaj sayısı**: `{total_messages}`"
         )
+
+@app.on_message(filters.command("id"))
+def handle_ids(_, message):
+    result = ids(_, message)
+    message.reply_text(result)
