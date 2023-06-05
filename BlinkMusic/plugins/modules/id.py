d0ecb417c8b27414b7e5f915528d270e4e58bff6
Heda <astroheda@gmail.com>
2023-06-05 14:29:22 +0300

    Update id.py

--- BlinkMusic/plugins/modules/id.py
+++ BlinkMusic/plugins/modules/id.py
@@ -1,19 +1,15 @@
 from BlinkMusic import app
 from pyrogram import filters
 
-def ids(_, message):
-    reply = message.reply_to_message
-    total_messages = app.get_chat_member(message.chat.id, message.from_user.id).user.total_messages
-    if reply:
-        return (
-            f"**Bu senin kimliğin**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: `{reply.from_user.id}`\n**Bu grubun kimliği**: `{message.chat.id}`\n**Toplam gönderdiğin mesaj sayısı**: `{total_messages}`"
-        )
-    else:
-        return (
-            f"**Bu senin kimliğin**: `{message.from_user.id}`\n**Bu grubun kimliği**: `{message.chat.id}`\n**Toplam gönderdiğin mesaj sayısı**: `{total_messages}`"
-        )
 
 @app.on_message(filters.command("id"))
-def handle_ids(_, message):
-    result = ids(_, message)
-    message.reply_text(result)
+def ids(_, message):
+    reply = message.reply_to_message
+    if reply:
+        message.reply_text(
+            f"Bu senin kimliğin: {message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: {reply.from_user.id}\n**Bu grubun kimliği**: {message.chat.id}`"
+        )
+    else:
+        message.reply(
+            f"Bu senin kimliğin: {message.from_user.id}`\n**Bu grubun kimliği**: {message.chat.id}`"
+        )
