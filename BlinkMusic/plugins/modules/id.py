429e0b8f437e36ad2bbc374787954e00e237951a
Heda <astroheda@gmail.com>
2023-06-05 14:24:55 +0300

    Update id.py

--- BlinkMusic/plugins/modules/id.py
+++ BlinkMusic/plugins/modules/id.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 
 def ids(_, message):
     reply = message.reply_to_message
-    total_messages = app.get_chat_members_count(message.chat.id)
+    total_messages = app.get_chat_member(message.chat.id, message.from_user.id).user.total_messages
     if reply:
         return (
             f"**Bu senin kimliğin**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: `{reply.from_user.id}`\n**Bu grubun kimliği**: `{message.chat.id}`\n**Toplam gönderdiğin mesaj sayısı**: `{total_messages}`"
