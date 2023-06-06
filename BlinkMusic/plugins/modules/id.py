9ab0fba8c3fa7327bd6ce495dbe7331e582f7305
Heda <astroheda@gmail.com>
2023-06-07 02:25:40 +0300

    Update id.py

--- BlinkMusic/plugins/modules/id.py
+++ BlinkMusic/plugins/modules/id.py
@@ -2,17 +2,41 @@ from BlinkMusic import app
 
 from pyrogram import filters
 
+user_message_count = {}
+
 @app.on_message(filters.command("id"))
 
 def ids(_, message):
+
+    user_id = message.from_user.id
+
+    chat_id = message.chat.id
+
+    # Kullanıcıyı uyar
+
+    if message.chat.type == "private":
+
+        message.reply_text("Bu komutu grup sohbetinde kullanmalısınız.")
+
+    # Mesajı yanıtlanan kullanıcının mesaj sayısını al
 
     reply = message.reply_to_message
 
     if reply:
 
+        replied_user_id = reply.from_user.id
+
+        if replied_user_id in user_message_count:
+
+            message_count = user_message_count[replied_user_id]
+
+        else:
+
+            message_count = 0
+
         message.reply_text(
 
-            f"Bu senin kimliğin: {message.from_user.id}\n{reply.from_user.first_name}'s ɪᴅ: {reply.from_user.id}\nBu grubun kimliği: {message.chat.id}"
+            f"Bu senin kimliğin: {user_id}\n{reply.from_user.first_name}'s ɪᴅ: {replied_user_id}\nBu grubun kimliği: {chat_id}\nMesaj sayısı: {message_count}"
 
         )
 
@@ -20,32 +44,30 @@ def ids(_, message):
 
         message.reply(
 
-            f"Bu senin kimliğin: {message.from_user.id}\nBu grubun kimliği: {message.chat.id}"
+            f"Bu senin kimliğin: {user_id}\nBu grubun kimliği: {chat_id}"
 
         )
 
-@app.on_message(filters.text & ~filters.private)
+@app.on_message(filters.text & ~filters.command)
 
-def count_messages(_, message):
+def update_message_count(_, message):
 
     user_id = message.from_user.id
 
-    chat_id = message.chat.id
+    if user_id in user_message_count:
 
-    user_data = db.get(f"{user_id}-{chat_id}")
+        user_message_count[user_id] += 1
 
-    if not user_data:
+    else:
 
-        user_data = {"message_count": 0}
+        user_message_count[user_id] = 1
 
-    user_data["message_count"] += 1
+@app.on_message(filters.left_chat_member)
 
-    db.set(f"{user_id}-{chat_id}", user_data)
+def remove_user_message_count(_, message):
 
-    
+    user_id = message.left_chat_member.id
 
-@app.on_message(filters.command("id", prefixes="/") & filters.private)
+    if user_id in user_message_count:
 
-def warn_user(_, message):
-
-    message.reply_text("Bu komutu özel bir sohbette kullanamazsın.")
+        del user_message_count[user_id]
