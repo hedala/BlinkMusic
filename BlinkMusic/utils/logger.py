843257dcd384ae71fe6a3b4b8dc901172379447c
Heda <astroheda@gmail.com>
2023-06-12 11:51:53 +0300

    Update logger.py

--- BlinkMusic/utils/logger.py
+++ BlinkMusic/utils/logger.py
@@ -8,25 +8,16 @@ async def play_logs(message, streamtype):
         if message.chat.username:
             chatusername = f"@{message.chat.username}"
         else:
-            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+            chatusername = "Özel Sohbet"
         logger_text = f"""
-**━━━━━━━━━━━━━━━**
-**💞 {MUSIC_BOT_NAME} ᴍᴜsɪᴄ ʟᴏɢs **
-**━━━━━━━━━━━━━━━**
-**🌹️ 𝐂𝐡𝐚𝐭 𝐍𝐚𝐌𝐞 : >** {message.chat.title} [`{message.chat.id}`]
-**━━━━━━━━━━━━━━━**
-**🥀 𝐍𝐚𝐌𝐞 : ›** {message.from_user.mention}
-**━━━━━━━━━━━━━━━**
-**🌸 𝐔𝐬𝐞𝐑𝐧𝐚𝐌𝐞 : ›** @{message.from_user.username}
-**━━━━━━━━━━━━━━━**
-**🌷 𝐈𝐃  : ›** `{message.from_user.id}`
-**━━━━━━━━━━━━━━━**
-**🌿 𝐂𝐡𝐚𝐭 𝐥𝐢𝐧𝐤: >** {chatusername}
-**━━━━━━━━━━━━━━━**
-**🌻 𝐒𝐞𝐀𝐫𝐜𝐇𝐞𝐝 𝐅𝐨𝐫:** {message.text}
-**━━━━━━━━━━━━━━━**
-**💐 𝐒𝐭𝐫𝐄𝐚𝐦 𝐓𝐲𝐏𝐞:** {streamtype}
-**━━━━━━━━━━━━━━━**"""
+** {MUSIC_BOT_NAME} Müzik Kayıtları **
+** Sohbet Adı :** {message.chat.title} [`{message.chat.id}`]
+** İsim :** {message.from_user.mention}
+** Kullanıcı Adı :** @{message.from_user.username}
+** Kimlik :** `{message.from_user.id}`
+** Chat Bağlantısı:** {chatusername}
+** Aranan İfade:** {message.text}
+** Yayın Türü:** {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
