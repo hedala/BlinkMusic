0636eb580ab0bf3dfe3df130814fe58af428c3c2
heda <103752316+hedala@users.noreply.github.com>
2023-06-16 16:00:15 +0300

    Update cp.py

--- BlinkMusic/plugins/modules/cp.py
+++ BlinkMusic/plugins/modules/cp.py
@@ -4,6 +4,10 @@ from pyrogram import filters
 import locale
 from datetime import datetime
 import pytz
+import time
+
+# Kripto fiyatlarını takip etmek için sözlük
+crypto_prices = {}
 
 @app.on_message(filters.command("coin"))
 def get_crypto_price(_, message):
@@ -58,10 +62,40 @@ def get_crypto_price(_, message):
             reply_text += f"\n\n**Güncelleme Zamanı:** {current_time}"
 
             message.reply_text(reply_text)
+
+            # Kripto biriminin fiyatını takip etmek için sözlüğe ekle
+            crypto_prices[crypto_id] = message.message_id
+            update_crypto_prices()
         else:
             message.reply_text("Hata: Fiyat bilgisi bulunamadı!")
     else:
         message.reply_text("Hata: Kripto birimi bulunamadı!")
+
+
+def update_crypto_prices():
+    # 5 dakika boyunca her 10 saniyede bir fiyatları güncelle
+    time.sleep(10)
+
+    if len(crypto_prices) > 0:
+        price_url = "https://api.coingecko.com/api/v3/simple/price"
+        ids = ','.join(crypto_prices.keys())
+        vs_currencies = "usd"
+        price_response = requests.get(f"{price_url}?ids={ids}&vs_currencies={vs_currencies}").json()
+
+        for crypto_id, message_id in crypto_prices.items():
+            if crypto_id in price_response:
+                crypto_price = price_response[crypto_id]["usd"]
+                formatted_price = locale.format_string("%.2f", crypto_price, grouping=True)
+                current_time = datetime.now().strftime("%H:%M:%S")
+
+                # Mesajı güncelle
+                app.update_message_text(message_id, f"**Güncel fiyat:** {formatted_price} USD\n\n**Güncelleme Zamanı:** {current_time}")
+
+        # 5 dakika süresi dolduktan sonra takip edilen kripto birimlerini sil
+        if time.time() - start_time >= 300:
+            crypto_prices.clear()
+    else:
+        return
 
 
 def format_large_number(number):
