fcfd3f41dfff77f73d25069b2c49d390190e30fb
heda <103752316+hedala@users.noreply.github.com>
2023-06-16 21:36:05 +0300

    Update cp.py

--- BlinkMusic/plugins/modules/cp.py
+++ BlinkMusic/plugins/modules/cp.py
@@ -4,6 +4,10 @@ from pyrogram import filters
 import locale
 from datetime import datetime
 import pytz
+import time
+
+# Kripto fiyatlarını takip etmek için sözlük
+crypto_prices = {}
 
 @app.on_message(filters.command("coin"))
 def get_crypto_price(_, message):
@@ -46,11 +50,11 @@ def get_crypto_price(_, message):
             formatted_market_cap = format_large_number(market_cap) if market_cap else None
             formatted_volume = format_large_number(volume) if volume else None
 
-            reply_text = f"{crypto_name} anlık fiyatı: {formatted_price} USD\n"
+            reply_text = f"**{crypto_name} anlık fiyatı:** {formatted_price} USD\n"
             if formatted_market_cap:
-                reply_text += f"{crypto_name} piyasa değeri: {formatted_market_cap} USD\n"
+                reply_text += f"**{crypto_name} piyasa değeri:** {formatted_market_cap} USD\n"
             if formatted_volume:
-                reply_text += f"{crypto_name} 24 saatlik işlem hacmi: {formatted_volume} USD"
+                reply_text += f"**{crypto_name} 24 saatlik işlem hacmi:** {formatted_volume} USD"
 
             # Anlık zamanı al ve mesajın sonuna ekle (Türkiye saati)
             istanbul_tz = pytz.timezone("Europe/Istanbul")
@@ -58,10 +62,37 @@ def get_crypto_price(_, message):
             reply_text += f"\n\n**Güncelleme Zamanı:** {current_time}"
 
             message.reply_text(reply_text)
+
+            # Kripto biriminin fiyatını takip etmek için sözlüğe ekle
+            crypto_prices[crypto_id] = message.message_id
+            update_crypto_prices()
         else:
             message.reply_text("Hata: Fiyat bilgisi bulunamadı!")
     else:
         message.reply_text("Hata: Kripto birimi bulunamadı!")
+
+
+def update_crypto_prices():
+    # 5 dakika boyunca her 10 saniyede bir fiyatları güncelle
+    start_time = time.time()
+    while time.time() - start_time < 300 and len(crypto_prices) > 0:
+        price_url = "https://api.coingecko.com/api/v3/simple/price"
+        ids = ','.join(crypto_prices.keys())
+        vs_currencies = "usd"
+        price_response = requests.get(f"{price_url}?ids={ids}&vs_currencies={vs_currencies}").json()
+
+        for crypto_id, message_id in crypto_prices.items():
+            if crypto_id in price_response:
+                crypto_price = price_response[crypto_id]["usd"]
+                formatted_price = locale.format_string("%.2f", crypto_price, grouping=True)
+                current_time = datetime.now().strftime("%H:%M:%S")
+
+                # Mesajı güncelle
+                app.edit_message_text(message_id, f"**Güncel fiyat:** {formatted_price} USD\n\n**Güncelleme Zamanı:** {current_time}")
+
+        # 5 dakika süresi dolduktan sonra takip edilen kripto birimlerini sil
+        if time.time() - start_time >= 300:
+            crypto_prices.clear()
 
 
 def format_large_number(number):
