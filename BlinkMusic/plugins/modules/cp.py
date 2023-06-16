759b96f57ca9c0760a9f9d79a8af7e3899d14b2b
heda <103752316+hedala@users.noreply.github.com>
2023-06-16 10:24:14 +0300

    Update cp.py

--- BlinkMusic/plugins/modules/cp.py
+++ BlinkMusic/plugins/modules/cp.py
@@ -5,13 +5,11 @@ from pyrogram import filters
 @app.on_message(filters.command("cp"))
 def get_crypto_price(_, message):
     crypto_symbol = message.text.split(" ", 1)[1].upper()  # İlk kelimeyi alıyoruz ve büyük harfe çeviriyoruz
-    url = f"https://api.coincap.io/v2/assets/{crypto_symbol}/"
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_symbol}&vs_currencies=usd"
     response = requests.get(url).json()
     
-    if "data" in response:
-        crypto_name = response["data"]["name"]
-        crypto_price = response["data"]["priceUsd"]
-        message.reply_text(f"{crypto_name} ({crypto_symbol}) anlık fiyatı: {crypto_price} USD")
+    if crypto_symbol in response:
+        crypto_price = response[crypto_symbol]["usd"]
+        message.reply_text(f"{crypto_symbol} anlık fiyatı: {crypto_price} USD")
     else:
         message.reply_text("Hata: Kripto birimi bulunamadı!")
-
