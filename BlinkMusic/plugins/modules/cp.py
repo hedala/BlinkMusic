0116cf54542111334f3cd06187a9976b396b371c
heda <103752316+hedala@users.noreply.github.com>
2023-06-16 13:59:36 +0300

    Update cp.py

--- BlinkMusic/plugins/modules/cp.py
+++ BlinkMusic/plugins/modules/cp.py
@@ -24,14 +24,27 @@ def get_crypto_price(_, message):
         if crypto_id.startswith("binance-peg-"):
             crypto_id = crypto_id.replace("binance-peg-", "")
         
-        url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies=usd"
-        response = requests.get(url).json()
+        # Kripto para birimi fiyatını al
+        price_url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies=usd"
+        price_response = requests.get(price_url).json()
         
-        if crypto_id in response:
-            crypto_price = response[crypto_id]["usd"]
+        if crypto_id in price_response:
+            crypto_price = price_response[crypto_id]["usd"]
             crypto_name = crypto_symbol.upper()
             message.reply_text(f"{crypto_name} anlık fiyatı: {crypto_price} USD")
         else:
             message.reply_text("Hata: Fiyat bilgisi bulunamadı!")
+        
+        # Kripto para birimi piyasa istatistiklerini al
+        stats_url = f"https://api.coingecko.com/api/v3/coins/{crypto_id}/market_statistics"
+        stats_response = requests.get(stats_url).json()
+        
+        if "market_cap" in stats_response and "volume" in stats_response:
+            market_cap = stats_response["market_cap"]["usd"]
+            volume = stats_response["volume"]["usd"]
+            message.reply_text(f"{crypto_name} piyasa değeri: {market_cap} USD\n"
+                               f"{crypto_name} 24 saatlik işlem hacmi: {volume} USD")
+        else:
+            message.reply_text("Hata: Piyasa istatistikleri bulunamadı!")
     else:
         message.reply_text("Hata: Kripto birimi bulunamadı!")
