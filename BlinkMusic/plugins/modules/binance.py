7335ad59d142af0568be49787eedac1a425b4930
heda <103752316+hedala@users.noreply.github.com>
2023-06-17 12:24:31 +0300

    Update binance.py

--- BlinkMusic/plugins/modules/binance.py
+++ BlinkMusic/plugins/modules/binance.py
@@ -1,30 +1,63 @@
+import requests
 from BlinkMusic import app
 from pyrogram import filters
-import requests
+import locale
+from datetime import datetime
+import pytz
 
 @app.on_message(filters.command("coin"))
-def coin_command(_, message):
-    symbol = "BTCUSDT"  # Örnek olarak BTCUSDT sembolünü kullanıyoruz, dilediğiniz sembolü burada belirtebilirsiniz
-    interval = "1d"  # Örnek olarak 1 günlük (1d) veri alıyoruz, dilediğiniz aralığı burada belirtebilirsiniz
-    
-    # Anlık fiyatı çekmek için API'yi kullanıyoruz
-    price_url = f"https://api3.binance.com/api/v3/ticker/price?symbol={symbol}"
-    price_response = requests.get(price_url)
-    price_data = price_response.json()
-    current_price = float(price_data.get("price", 0))
-    
-    # Fiyat değişimini çekmek için API'yi kullanıyoruz
-    klines_url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}"
-    klines_response = requests.get(klines_url)
-    klines_data = klines_response.json()
-    
-    if isinstance(klines_data, list) and len(klines_data) > 0:
-        price_change = float(klines_data[-1][4]) - float(klines_data[0][4])
+def get_crypto_price(_, message):
+    crypto_symbol = message.text.split(" ", 1)[1].lower()
+
+    url = f"https://api3.binance.com/api/v3/ticker/price?symbol={crypto_symbol.upper()}USDT"
+    response = requests.get(url).json()
+
+    if "symbol" in response and "price" in response:
+        crypto_price = response["price"]
+        crypto_name = crypto_symbol.upper()
+
+        # Anlık zamanı al ve mesajın sonuna ekle (Türkiye saati)
+        istanbul_tz = pytz.timezone("Europe/Istanbul")
+        current_time = datetime.now(istanbul_tz).strftime("%Y-%m-%d %H:%M:%S")
+
+        reply_text = f"{crypto_name} anlık fiyatı: {crypto_price} USDT\n\nGüncelleme Zamanı: {current_time}"
+        message.reply_text(reply_text)
     else:
-        price_change = 0
-    
-    message.reply_text(
-        f"**{symbol} Coin Bilgileri**\n"
-        f"Anlık Fiyat: {price}\n"
-        f"Fiyat Değişimi: {price_change}"
-    )
+        message.reply_text("Hata: Fiyat bilgisi bulunamadı!")
+
+
+@app.on_message(filters.command("stats"))
+def get_crypto_stats(_, message):
+    crypto_symbol = message.text.split(" ", 1)[1].lower()
+
+    url = f"https://api.coingecko.com/api/v3/coins/{crypto_symbol}"
+    response = requests.get(url).json()
+
+    if "market_data" in response and "market_cap" in response["market_data"] and "total_volume" in response["market_data"]:
+        market_cap = response["market_data"]["market_cap"]["usd"]
+        volume = response["market_data"]["total_volume"]["usd"]
+
+        # Sayıları okunaklı bir şekilde formatla
+        formatted_market_cap = format_large_number(market_cap) if market_cap else None
+        formatted_volume = format_large_number(volume) if volume else None
+
+        reply_text = f"{crypto_symbol.upper()} piyasa değeri: {formatted_market_cap} USD\n" if formatted_market_cap else ""
+        reply_text += f"{crypto_symbol.upper()} 24 saatlik işlem hacmi: {formatted_volume} USD" if formatted_volume else ""
+
+        message.reply_text(reply_text)
+    else:
+        message.reply_text("Hata: Piyasa istatistikleri bulunamadı!")
+
+
+def format_large_number(number):
+    if number is None:
+        return None
+
+    if abs(number) >= 1_000_000_000:
+        formatted_number = f"{number / 1_000_000_000:.2f}B"
+    elif abs(number) >= 1_000_000:
+        formatted_number = f"{number / 1_000_000:.2f}M"
+    else:
+        formatted_number = f"{number:,.2f}"
+
+    return formatted_number
