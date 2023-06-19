33f202fdd2ed56075c3d55ae4a9cd80576bc2d79
heda <103752316+hedala@users.noreply.github.com>
2023-06-19 12:52:58 +0300

    Update binance.py

--- BlinkMusic/plugins/modules/binance.py
+++ BlinkMusic/plugins/modules/binance.py
@@ -3,22 +3,17 @@ from BlinkMusic import app
 from pyrogram import filters
 
 
-def get_crypto_price(symbol):
-    url = f"https://api3.binance.com/api/v3/ticker/price?symbol={symbol}"
+@app.on_message(filters.command("price"))
+def get_price(_, message):
+    symbol = message.text.split()[1].upper()
+    url = f"https://api3.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         if 'price' in data:
-            return data['price']
-    return None
+            price = data['price']
+            message.reply_text(f"{symbol} fiyatı: {price}")
+            return
+    message.reply_text(f"{symbol} fiyatı bulunamadı.")
 
-
-@app.on_message(filters.command("price"))
-def get_price(_, message):
-    symbol = message.text.split()[1].upper()
-    price = get_crypto_price(symbol + "USDT")
-    if price:
-        message.reply_text(f"{symbol} fiyatı: {price}")
-    else:
-        message.reply_text(f"{symbol} fiyatı bulunamadı.")
 
