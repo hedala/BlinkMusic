1239360343c78bd13fbbea9d8223cb23a76f6e1c
heda <103752316+hedala@users.noreply.github.com>
2023-06-19 15:02:08 +0300

    Update sildim.py

--- BlinkMusic/plugins/modules/sildim.py
+++ BlinkMusic/plugins/modules/sildim.py
@@ -7,11 +7,12 @@ def delete_messages(_, message):
     if message.reply_to_message:
         chat_id = message.chat.id
         reply_message_id = message.reply_to_message.message_id
+        message_count = message.message_id - reply_message_id + 1
 
         if app.get_chat_member(chat_id, message.from_user.id).status in ("creator", "administrator"):
             try:
                 app.delete_messages(chat_id, range(reply_message_id, message.message_id + 1))
-                message.reply_text("Mesajlar silindi.")
+                message.reply_text(f"{message_count} mesaj silindi.")
             except Exception as e:
                 message.reply_text("Mesajları silerken bir hata oluştu.")
                 app.send_message(message.from_user.id, f"Silme işlemi sırasında bir hata oluştu: {str(e)}")
