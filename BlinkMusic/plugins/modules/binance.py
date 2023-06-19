dc6c4c44036abc580db32d56652a00c429d7bbe3
heda <103752316+hedala@users.noreply.github.com>
2023-06-19 12:44:09 +0300

    Update binance.py

--- BlinkMusic/plugins/modules/binance.py
+++ BlinkMusic/plugins/modules/binance.py
@@ -2,20 +2,21 @@ import requests
 from BlinkMusic import app
 from pyrogram import filters
 
-binance_api_url = "https://api3.binance.com/api/v3/ticker/price"
+# Yeni özellik: Binance API'den anlık kripto fiyatlarını çekme
+def get_crypto_price(symbol):
+    url = f"https://api3.binance.com/api/v3/ticker/price?symbol={symbol}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if 'price' in data:
+            return data['price']
+    return None
 
-
-@app.on_message(filters.command("fiyat"))
-def get_crypto_price(_, message):
-    try:
-        response = requests.get(binance_api_url)
-        response_json = response.json()
-        btc_price = next(item for item in response_json if item["symbol"] == "BTCUSDT")["price"]
-        eth_price = next(item for item in response_json if item["symbol"] == "ETHUSDT")["price"]
-        
-        message.reply_text(
-            f"BTC fiyatı: {btc_price}\nETH fiyatı: {eth_price}"
-        )
-    except Exception as e:
-        message.reply_text("Kripto fiyatlarını alırken bir hata oluştu. Lütfen daha sonra tekrar deneyin.")
-        print(f"Hata: {str(e)}")
+@app.on_message(filters.command("price"))
+def get_price(_, message):
+    symbol = message.text.split()[1].upper()
+    price = get_crypto_price(symbol)
+    if price:
+        message.reply_text(f"{symbol} fiyatı: {price}")
+    else:
+        message.reply_text(f"{symbol} fiyatı bulunamadı.")
