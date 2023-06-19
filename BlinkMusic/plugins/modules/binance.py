bba9794a1a5a28066e37c982ba39e193a444053c
heda <103752316+hedala@users.noreply.github.com>
2023-06-19 13:31:48 +0300

    Update binance.py

--- BlinkMusic/plugins/modules/binance.py
+++ BlinkMusic/plugins/modules/binance.py
@@ -2,12 +2,16 @@ from BlinkMusic import app
 from pyrogram import filters
 import requests
 
-def get_crypto_price(symbol):
-    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol.upper()}USDT"
-    response = requests.get(url)
-    data = response.json()
-    price = float(data["price"])
-    return price
+   def get_crypto_price(symbol):
+       url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol.upper()}USDT"
+       response = requests.get(url)
+       data = response.json()
+       try:
+           price = float(data["price"])
+           return price
+       except KeyError:
+           # Handle the case where the 'price' key is not present in the response
+           return None
 
 @app.on_message(filters.command("coin"))
 def coin(_, message):
