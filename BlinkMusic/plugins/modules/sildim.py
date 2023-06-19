7e3761cf9c69cb6f14ae286bf87e460ff99da2da
heda <103752316+hedala@users.noreply.github.com>
2023-06-19 14:57:52 +0300

    Update sildim.py

--- BlinkMusic/plugins/modules/sildim.py
+++ BlinkMusic/plugins/modules/sildim.py
@@ -2,25 +2,19 @@ from BlinkMusic import app
 from pyrogram import filters
 
 
-def is_group_admin(chat_id, user_id):
-    chat_member = app.get_chat_member(chat_id, user_id)
-    return chat_member.status in ("owner", "administrator")
-
-
 @app.on_message(filters.command("sil") & filters.group)
 def delete_messages(_, message):
     if message.reply_to_message:
         chat_id = message.chat.id
         reply_message_id = message.reply_to_message.message_id
-        user_id = message.from_user.id
 
-        if is_group_admin(chat_id, user_id) or user_id == app.get_me().id:
+        if app.get_chat_member(chat_id, message.from_user.id).status in ("creator", "administrator"):
             try:
                 app.delete_messages(chat_id, range(reply_message_id, message.message_id + 1))
                 message.reply_text("Mesajlar silindi.")
             except Exception as e:
                 message.reply_text("Mesajları silerken bir hata oluştu.")
-                app.send_message(user_id, f"Silme işlemi sırasında bir hata oluştu: {str(e)}")
+                app.send_message(message.from_user.id, f"Silme işlemi sırasında bir hata oluştu: {str(e)}")
         else:
             message.reply_text("Bu komutu kullanma yetkiniz bulunmuyor.")
     else:
