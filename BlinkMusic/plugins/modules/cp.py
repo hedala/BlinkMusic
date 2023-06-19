5aa2070c5726c217b1361a28fd758beffcfaf13a
heda <103752316+hedala@users.noreply.github.com>
2023-06-19 14:37:09 +0300

    Update cp.py

--- BlinkMusic/plugins/modules/cp.py
+++ BlinkMusic/plugins/modules/cp.py
@@ -4,83 +4,77 @@ from pyrogram import filters
 import locale
 from datetime import datetime
 import pytz
-from cachetools import cached, TTLCache
 
-# Önbellekleme için önbellek nesnesini oluştur
-cache = TTLCache(maxsize=100, ttl=300)  # Maksimum 100 öğeyle 5 dakika süreyle önbellekleyin
+# Kripto fiyatlarını önbellekte saklamak için bir sözlük kullanalım
+cache = {}
 
-@app.on_message(filters.command("coin"))
-def get_crypto_price(_, message):
-    crypto_symbol = message.text.split(" ", 1)[1].lower()
-
-    crypto_price = get_cached_price(crypto_symbol)
-    if crypto_price:
-        send_reply_message(message, crypto_symbol, crypto_price)
-    else:
-        response = requests.get("https://api.coingecko.com/api/v3/coins/list").json()
-
-        crypto_id = None
-
-        if isinstance(response, list):
-            for crypto in response:
-                if crypto.get("symbol") == crypto_symbol:
-                    crypto_id = crypto["id"]
-                    break
-        else:
-            return
-
-        if crypto_id:
-            if crypto_id.startswith("binance-peg-"):
-                crypto_id = crypto_id.replace("binance-peg-", "")
-
-            price_url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies=usd"
-            stats_url = f"https://api.coingecko.com/api/v3/coins/{crypto_id}"
-
-            price_response = requests.get(price_url).json()
-            stats_response = requests.get(stats_url).json()
-
-            if crypto_id in price_response:
-                crypto_price = price_response[crypto_id]["usd"]
-                crypto_name = crypto_symbol.upper()
-
-                market_cap = stats_response.get("market_data", {}).get("market_cap", {}).get("usd")
-                volume = stats_response.get("market_data", {}).get("total_volume", {}).get("usd")
-
-                # Sayıları okunaklı bir şekilde formatla
-                locale.setlocale(locale.LC_ALL, "C")
-                formatted_price = locale.format_string("%.2f", crypto_price, grouping=True)
-                formatted_market_cap = format_large_number(market_cap) if market_cap else None
-                formatted_volume = format_large_number(volume) if volume else None
-
-                reply_text = f"{crypto_name} anlık fiyatı: {formatted_price} USD\n"
-                if formatted_market_cap:
-                    reply_text += f"{crypto_name} piyasa değeri: {formatted_market_cap} USD\n"
-                if formatted_volume:
-                    reply_text += f"{crypto_name} 24 saatlik işlem hacmi: {formatted_volume} USD"
-
-                # Anlık zamanı al ve mesajın sonuna ekle (Türkiye saati)
-                istanbul_tz = pytz.timezone("Europe/Istanbul")
-                current_time = datetime.now(istanbul_tz).strftime("%H:%M:%S")
-                reply_text += f"\n\n**Güncelleme Zamanı:** {current_time}"
-
-                # Önbelleğe fiyatı ekle
-                add_price_to_cache(crypto_symbol, crypto_price)
-
-                send_reply_message(message, crypto_symbol, reply_text)
-            else:
-                message.reply_text("Hata: Fiyat bilgisi bulunamadı!")
-        else:
-            message.reply_text("Hata: Kripto birimi bulunamadı!")
-
-# Önbelleğe fiyatı ekleyen yardımcı fonksiyon
+# Kripto fiyatını önbelleğe ekleyen yardımcı fonksiyon
 def add_price_to_cache(crypto_symbol, crypto_price):
     cache[crypto_symbol] = crypto_price
 
 # Önbellekten fiyatı alıp döndüren yardımcı fonksiyon
-@cached(cache)
 def get_cached_price(crypto_symbol):
     return cache.get(crypto_symbol)
 
 # Mesaj yanıtını gönderen yardımcı fonksiyon
 def send_reply_message(message, crypto_symbol, reply_text):
     message.reply_text(reply_text)
+
+@app.on_message(filters.command("coin"))
+def get_crypto_price(_, message):
+    crypto_symbol = message.text.split(" ", 1)[1].lower()
+
+    url = "https://api.coingecko.com/api/v3/coins/list"
+    response = requests.get(url).json()
+
+    crypto_id = None
+
+    if isinstance(response, list):
+        for crypto in response:
+            if crypto.get("symbol") == crypto_symbol:
+                crypto_id = crypto["id"]
+                break
+    else:
+        send_reply_message(message, crypto_symbol, "Hata: Geçersiz API yanıtı!")
+        return
+
+    if crypto_id:
+        if crypto_id.startswith("binance-peg-"):
+            crypto_id = crypto_id.replace("binance-peg-", "")
+
+        price_url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies=usd"
+        stats_url = f"https://api.coingecko.com/api/v3/coins/{crypto_id}"
+
+        price_response = requests.get(price_url).json()
+        stats_response = requests.get(stats_url).json()
+
+        if crypto_id in price_response:
+            crypto_price = price_response[crypto_id]["usd"]
+            crypto_name = crypto_symbol.upper()
+
+            market_cap = stats_response.get("market_data", {}).get("market_cap", {}).get("usd")
+            volume = stats_response.get("market_data", {}).get("total_volume", {}).get("usd")
+
+            # Sayıları okunaklı bir şekilde formatla
+            locale.setlocale(locale.LC_ALL, "C")
+            formatted_price = locale.format_string("%.2f", crypto_price, grouping=True)
+            formatted_market_cap = format_large_number(market_cap) if market_cap else None
+            formatted_volume = format_large_number(volume) if volume else None
+
+            reply_text = f"{crypto_name} anlık fiyatı: {formatted_price} USD\n"
+            if formatted_market_cap:
+                reply_text += f"{crypto_name} piyasa değeri: {formatted_market_cap} USD\n"
+            if formatted_volume:
+                reply_text += f"{crypto_name} 24 saatlik işlem hacmi: {formatted_volume} USD"
+
+            # Anlık zamanı al ve mesajın sonuna ekle (Türkiye saati)
+            istanbul_tz = pytz.timezone("Europe/Istanbul")
+            current_time = datetime.now(istanbul_tz).strftime("%H:%M:%S")
+            reply_text += f"\n\nGüncellenme zamanı: {current_time}"
+
+            add_price_to_cache(crypto_symbol, crypto_price)
+            send_reply_message(message, crypto_symbol, reply_text)
+        else:
+            send_reply_message(message, crypto_symbol, "Hata: Geçersiz API yanıtı!")
+    else:
+        send_reply_message(message, crypto_symbol, f"{crypto_symbol.upper()} simgesi bulunamadı!")
