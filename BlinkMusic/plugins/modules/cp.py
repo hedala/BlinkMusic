55ff7cfa49e81107cfdeb9776ce04e927b9cda8d
heda <103752316+hedala@users.noreply.github.com>
2023-06-19 13:54:41 +0300

    Update cp.py

--- BlinkMusic/plugins/modules/cp.py
+++ BlinkMusic/plugins/modules/cp.py
@@ -5,7 +5,7 @@ import locale
 from datetime import datetime
 import pytz
 
-@app.on_message(filters.command("coinnn"))
+@app.on_message(filters.command("coin"))
 def get_crypto_price(_, message):
     crypto_symbol = message.text.split(" ", 1)[1].lower()
 
@@ -29,13 +29,9 @@ def get_crypto_price(_, message):
 
         price_url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies=usd"
         stats_url = f"https://api.coingecko.com/api/v3/coins/{crypto_id}"
-        chart_url = f"https://api.coingecko.com/api/v3/coins/{crypto_id}/market_chart"
 
         price_response = requests.get(price_url).json()
         stats_response = requests.get(stats_url).json()
-        chart_response_24h = requests.get(chart_url, params={"vs_currency": "usd", "days": "1"}).json()
-        chart_response_1h = requests.get(chart_url, params={"vs_currency": "usd", "hours": "1"}).json()
-        chart_response_1m = requests.get(chart_url, params={"vs_currency": "usd", "minutes": "1"}).json()
 
         if crypto_id in price_response:
             crypto_price = price_response[crypto_id]["usd"]
@@ -54,34 +50,12 @@ def get_crypto_price(_, message):
             if formatted_market_cap:
                 reply_text += f"{crypto_name} piyasa değeri: {formatted_market_cap} USD\n"
             if formatted_volume:
-                reply_text += f"{crypto_name} 24 saatlik işlem hacmi: {formatted_volume} USD\n"
-
-            # Değişim yüzdelerini al ve yanıta ekle
-            price_data_24h = chart_response_24h.get("prices", [])
-            if len(price_data_24h) > 1:
-                start_price = price_data_24h[0][1]
-                end_price = price_data_24h[-1][1]
-                price_change_percentage_24h = ((end_price - start_price) / start_price) * 100
-                reply_text += f"{crypto_name} son 24 saatlik değişim yüzdesi: {price_change_percentage_24h:.2f}%\n"
-
-            price_data_1h = chart_response_1h.get("prices", [])
-            if len(price_data_1h) > 1:
-                start_price = price_data_1h[0][1]
-                end_price = price_data_1h[-1][1]
-                price_change_percentage_1h = ((end_price - start_price) / start_price) * 100
-                reply_text += f"{crypto_name} son 1 saatlik değişim yüzdesi: {price_change_percentage_1h:.2f}%\n"
-
-            price_data_1m = chart_response_1m.get("prices", [])
-            if len(price_data_1m) > 1:
-                start_price = price_data_1m[0][1]
-                end_price = price_data_1m[-1][1]
-                price_change_percentage_1m = ((end_price - start_price) / start_price) * 100
-                reply_text += f"{crypto_name} son 1 dakikalık değişim yüzdesi: {price_change_percentage_1m:.2f}%\n"
+                reply_text += f"{crypto_name} 24 saatlik işlem hacmi: {formatted_volume} USD"
 
             # Anlık zamanı al ve mesajın sonuna ekle (Türkiye saati)
             istanbul_tz = pytz.timezone("Europe/Istanbul")
             current_time = datetime.now(istanbul_tz).strftime("%H:%M:%S")
-            reply_text += f"\n**Güncelleme Zamanı:** {current_time}"
+            reply_text += f"\n\n**Güncelleme Zamanı:** {current_time}"
 
             message.reply_text(reply_text)
         else:
