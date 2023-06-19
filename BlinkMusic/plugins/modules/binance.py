f0a814a7ae3a6ba96d0dc4b40b4a208b8aaeb3ed
heda <103752316+hedala@users.noreply.github.com>
2023-06-19 13:49:59 +0300

    Update binance.py

--- BlinkMusic/plugins/modules/binance.py
+++ BlinkMusic/plugins/modules/binance.py
@@ -2,38 +2,21 @@ from BlinkMusic import app
 from pyrogram import filters
 import requests
 
-def get_crypto_stats(symbol):
-    url = f"https://api.binance.com/api/v3/ticker/24hr?symbol={symbol.upper()}USDT"
+def get_crypto_price():
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
     response = requests.get(url)
     data = response.json()
     try:
-        stats = {
-            "symbol": data["symbol"],
-            "price_change": float(data["priceChange"]),
-            "price_change_percent": float(data["priceChangePercent"]),
-            "high_price": float(data["highPrice"]),
-            "low_price": float(data["lowPrice"]),
-            "volume": float(data["volume"]),
-            "quote_volume": float(data["quoteVolume"])
-        }
-        return stats
+        price = float(data["price"])
+        return price
     except KeyError:
         return None
 
 @app.on_message(filters.command("coin"))
 def coin(_, message):
-    symbol = message.text.split(" ")[1]
-    stats = get_crypto_stats(symbol)
-    if stats is not None:
-        reply_text = f"""
-        {stats['symbol'].upper()} fiyat istatistikleri:
-        Değişim: {stats['price_change']} USDT
-        Değişim Yüzdesi: {stats['price_change_percent']}%
-        En Yüksek Fiyat: {stats['high_price']} USDT
-        En Düşük Fiyat: {stats['low_price']} USDT
-        Hacim: {stats['volume']}
-        Alıntı Hacmi: {stats['quote_volume']}
-        """
+    price = get_crypto_price()
+    if price is not None:
+        reply_text = f"BTCUSDT fiyatı: {price} USDT"
     else:
-        reply_text = f"{symbol.upper()} fiyat istatistikleri alınamadı."
+        reply_text = "BTCUSDT fiyatı alınamadı."
     message.reply_text(reply_text)
