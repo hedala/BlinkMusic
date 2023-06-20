c7c87d185e0266651595bcfaad3d847c54e3c4f3
heda <103752316+hedala@users.noreply.github.com>
2023-06-20 10:46:04 +0300

    Update pic.py

--- BlinkMusic/plugins/modules/pic.py
+++ BlinkMusic/plugins/modules/pic.py
@@ -7,15 +7,34 @@ def send_profile_photo(_, message):
     if reply:
         user_id = reply.from_user.id
         chat_id = message.chat.id
-        
-        # Kullanıcının profil fotoğrafını almak için get_chat_member metodu kullanılır.
         member = app.get_chat_member(chat_id, user_id)
         if member and member.user and member.user.photo:
             photo = member.user.photo.big_file_id
-            # Profil fotoğrafını indirmek için download_media metodu kullanılır.
             downloaded_photo = app.download_media(photo)
-            # Profil fotoğrafını göndermek için send_photo metodunu kullanabilirsiniz
-            app.send_photo(chat_id, downloaded_photo)
+            caption = f"{reply.from_user.first_name}'ın profil fotoğrafı:"
+            app.send_photo(chat_id, downloaded_photo, caption=caption)
+        else:
+            app.send_message(chat_id, "Bu kullanıcının profil fotoğrafı bulunamadı.")
+    else:
+        app.send_message(chat_id, "Bir kullanıcıya yanıt vererek bu komutu kullanmalısınız.")
+
+@app.on_message(filters.command("picall"))
+def send_all_profile_photos(_, message):
+    reply = message.reply_to_message
+    if reply:
+        user_id = reply.from_user.id
+        chat_id = message.chat.id
+        member = app.get_chat_member(chat_id, user_id)
+        if member and member.user and member.user.photo:
+            photos = app.get_user_profile_photos(user_id).photos
+            if photos:
+                app.send_message(chat_id, f"{len(photos)} adet fotoğraf buldum. Hemen indirip iletiyorum efendi {reply.from_user.first_name}.")
+                for index, photo in enumerate(photos):
+                    downloaded_photo = app.download_media(photo.file_id)
+                    caption = f"{reply.from_user.first_name}'ın {index+1}. profil fotoğrafı:"
+                    app.send_photo(chat_id, downloaded_photo, caption=caption)
+            else:
+                app.send_message(chat_id, "Bu kullanıcının profil fotoğrafı bulunamadı.")
         else:
             app.send_message(chat_id, "Bu kullanıcının profil fotoğrafı bulunamadı.")
     else:
