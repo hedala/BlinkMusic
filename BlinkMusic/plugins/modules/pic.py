b1f23f93a8be1622130c046364f28e992cf089c7
heda <103752316+hedala@users.noreply.github.com>
2023-06-20 10:50:42 +0300

    Update pic.py

--- BlinkMusic/plugins/modules/pic.py
+++ BlinkMusic/plugins/modules/pic.py
@@ -11,7 +11,7 @@ def send_profile_photo(_, message):
         if member and member.user and member.user.photo:
             photo = member.user.photo.big_file_id
             downloaded_photo = app.download_media(photo)
-            caption = f"{reply.from_user.first_name}'nın profil fotoğrafı:"
+            caption = f"{reply.from_user.first_name}'ın profil fotoğrafı:"
             app.send_photo(chat_id, downloaded_photo, caption=caption)
         else:
             app.send_message(chat_id, "Bu kullanıcının profil fotoğrafı bulunamadı.")
@@ -26,16 +26,19 @@ def send_all_profile_photos(_, message):
         chat_id = message.chat.id
         member = app.get_chat_member(chat_id, user_id)
         if member and member.user and member.user.photo:
-            photos = app.get_user_profile_photos(user_id)
-            if photos.total_count > 0:
-                app.send_message(chat_id, f"{photos.total_count} adet fotoğraf buldum. Hemen indirip iletiyorum efendi {reply.from_user.first_name}.")
-                for photo in photos.photos:
-                    file_id = photo[0].file_id
+            total_count = app.get_chat_members_count(chat_id)
+            app.send_message(chat_id, f"{total_count} adet fotoğraf buldum. Hemen indirip iletiyorum efendi {reply.from_user.first_name}.")
+            offset = 0
+            while True:
+                photos = app.get_chat_member(chat_id, user_id, offset=offset, limit=100).user.photos
+                for photo in photos:
+                    file_id = photo.big_file_id
                     downloaded_photo = app.download_media(file_id)
                     caption = f"{reply.from_user.first_name}'ın profil fotoğrafı:"
                     app.send_photo(chat_id, downloaded_photo, caption=caption)
-            else:
-                app.send_message(chat_id, "Bu kullanıcının profil fotoğrafı bulunamadı.")
+                offset += len(photos)
+                if len(photos) < 100:
+                    break
         else:
             app.send_message(chat_id, "Bu kullanıcının profil fotoğrafı bulunamadı.")
     else:
