78c7d6bf10b07f0e2b7f73df245c7e5fd893727a
heda <103752316+hedala@users.noreply.github.com>
2023-06-20 10:32:27 +0300

    Update pic.py

--- BlinkMusic/plugins/modules/pic.py
+++ BlinkMusic/plugins/modules/pic.py
@@ -8,14 +8,13 @@ def send_profile_photo(_, message):
         user_id = reply.from_user.id
         chat_id = message.chat.id
         
-        # Kullanıcının profil fotoğrafını almak için get_user_profile_photos metodunu kullanabilirsiniz
+        # Kullanıcının profil fotoğrafını almak için getUserProfilePhotos metodu kullanılmalıdır.
         photos = app.get_user_profile_photos(user_id, limit=1)
-        if photos.total_count > 0:
-            photo = photos.photos[0][0]
+        if photos and len(photos.photos) > 0:
+            photo = app.get_file(photos.photos[0].file_id)
             # Profil fotoğrafını göndermek için send_photo metodunu kullanabilirsiniz
             app.send_photo(chat_id, photo.file_id)
         else:
             app.send_message(chat_id, "Bu kullanıcının profil fotoğrafı bulunamadı.")
     else:
         app.send_message(chat_id, "Bir kullanıcıya yanıt vererek bu komutu kullanmalısınız.")
-
