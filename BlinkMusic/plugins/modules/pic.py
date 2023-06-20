4aec398996f7ab73346f47d996b99891b808929f
heda <103752316+hedala@users.noreply.github.com>
2023-06-20 10:55:46 +0300

    Update pic.py

--- BlinkMusic/plugins/modules/pic.py
+++ BlinkMusic/plugins/modules/pic.py
@@ -28,17 +28,20 @@ def send_all_profile_photos(_, message):
         if member and member.user and member.user.photo:
             total_count = app.get_chat_members_count(chat_id)
             app.send_message(chat_id, f"{total_count} adet fotoğraf buldum. Hemen indirip iletiyorum efendi {reply.from_user.first_name}.")
-            offset = 0
+            offset = None
             while True:
-                photos = app.get_chat_member(chat_id, user_id, offset=offset, limit=100).user.photos
-                for photo in photos:
-                    file_id = photo.big_file_id
-                    downloaded_photo = app.download_media(file_id)
-                    caption = f"{reply.from_user.first_name}'ın profil fotoğrafı:"
-                    app.send_photo(chat_id, downloaded_photo, caption=caption)
-                offset += len(photos)
-                if len(photos) < 100:
+                members = app.get_chat_members(chat_id, limit=100, offset=offset)
+                for member in members:
+                    if member.user and member.user.photo:
+                        photos = app.get_user_profile_photos(member.user.id).photos
+                        for photo in photos:
+                            file_id = photo[-1].file_id
+                            downloaded_photo = app.download_media(file_id)
+                            caption = f"{member.user.first_name}'ın profil fotoğrafı:"
+                            app.send_photo(chat_id, downloaded_photo, caption=caption)
+                if len(members) < 100:
                     break
+                offset = members[-1].user.id
         else:
             app.send_message(chat_id, "Bu kullanıcının profil fotoğrafı bulunamadı.")
     else:
