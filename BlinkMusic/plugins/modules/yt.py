81260fecf2449a93a1e2686e009f4c70d9f8a279
heda <103752316+hedala@users.noreply.github.com>
2023-06-22 14:06:28 +0300

    Update yt.py

--- BlinkMusic/plugins/modules/yt.py
+++ BlinkMusic/plugins/modules/yt.py
@@ -3,10 +3,10 @@ from pyrogram import filters
 import os
 from pytube import YouTube
 
-@app.on_message(filters.command("yt"))
-def youtube_download(_, message):
+@app.on_message(filters.command("yts"))
+def youtube_video_download(_, message):
     url = message.text.split(" ", 1)[1]
-    
+
     try:
         video = YouTube(url)
         video_stream = video.streams.get_highest_resolution()
@@ -15,9 +15,30 @@ def youtube_download(_, message):
         video_stream.download()
         
         # İndirilen videoyu kaydetme
-        new_filename = video_stream.default_filename
-        os.rename(new_filename, f"{message.from_user.id}_{new_filename}")
-        message.reply_video(f"{message.from_user.id}_{new_filename}")
+        new_filename = f"{message.from_user.id}_{video_stream.default_filename}"
+        os.rename(video_stream.default_filename, new_filename)
+        
+        message.reply_video(new_filename)
         message.reply_text("Video başarıyla indirildi.")
     except Exception as e:
         message.reply_text("Video indirme hatası: " + str(e))
+
+@app.on_message(filters.command("yta"))
+def youtube_audio_download(_, message):
+    url = message.text.split(" ", 1)[1]
+
+    try:
+        video = YouTube(url)
+        audio_stream = video.streams.filter(only_audio=True).first()
+        
+        # Ses parçasını indirme
+        audio_stream.download()
+        
+        # İndirilen ses parçasını kaydetme
+        new_filename = f"{message.from_user.id}_{audio_stream.default_filename}"
+        os.rename(audio_stream.default_filename, new_filename)
+        
+        message.reply_audio(new_filename)
+        message.reply_text("Ses parçası başarıyla indirildi.")
+    except Exception as e:
+        message.reply_text("Ses parçası indirme hatası: " + str(e))
