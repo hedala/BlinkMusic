e9d846d86e561ee6345460a22069765e67283b3e
heda <103752316+hedala@users.noreply.github.com>
2023-06-23 01:45:51 +0300

    Update ohayo.py

--- BlinkMusic/plugins/modules/ohayo.py
+++ BlinkMusic/plugins/modules/ohayo.py
@@ -2,7 +2,7 @@ import requests
 from BlinkMusic import app
 from pyrogram import filters
 
-@app.on_message(filters.command("coin"))
+@app.on_message(filters.command("hel"))
 def show_coin_price(_, message):
     symbol = message.text.upper().split()[1]  # İlk kelimeyi komut olarak alıyoruz, ikinci kelimeyi sembol olarak alıyoruz
     response = requests.get(f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}")
