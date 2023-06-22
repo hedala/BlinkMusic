be9fde0ff156fdc31ececa65713e253aeb7eb26f
heda <103752316+hedala@users.noreply.github.com>
2023-06-22 15:08:19 +0300

    Update yt.py

--- BlinkMusic/plugins/modules/yt.py
+++ BlinkMusic/plugins/modules/yt.py
@@ -2,6 +2,7 @@ from BlinkMusic import app
 from pyrogram import filters
 import os
 from pytube import YouTube
+from requests import get
 
 @app.on_message(filters.command("yt"))
 def youtube_download(_, message):
@@ -17,7 +18,13 @@ def youtube_download(_, message):
         # İndirilen videoyu kaydetme
         new_filename = video_stream.default_filename
         os.rename(new_filename, f"{message.from_user.id}_{new_filename}")
-        message.reply_video(f"{message.from_user.id}_{new_filename}", duration=video.length, thumb=video.thumbnail_url)
+
+        request = get(video.thumbnail_url)
+        thumb_file = f"{message.from_user.id}_{new_filename}.jpg"
+        with open(thumb_file, "wb") as file:
+            file.write(request.content)
+
+        message.reply_video(f"{message.from_user.id}_{new_filename}", duration=video.length, thumb=thumb_file)
         message.reply_text("Video başarıyla indirildi.")
     except Exception as e:
         message.reply_text("Video indirme hatası: " + str(e))
