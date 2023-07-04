68d82aef6aaf4b75da621e37c133a37d9a5c850c
heda <103752316+hedala@users.noreply.github.com>
2023-07-04 11:05:42 +0300

    Update lia.py

--- BlinkMusic/plugins/modules/lia.py
+++ BlinkMusic/plugins/modules/lia.py
@@ -3,13 +3,17 @@ import os
 from BlinkMusic import app
 from pyrogram import filters
 
+DEEZER_ACCESS_TOKEN = "f9c01ce26c909b78b6563451092bf1a4"
 
 @app.on_message(filters.command("deezer"))
 def download_music(_, message):
     query = message.text.split(" ", 1)[1]  # Kullanıcının gönderdiği mesajdan sorguyu alıyoruz
 
     # Deezer API'ye istek atarak müzikleri arıyoruz
-    response = requests.get(f"https://api.deezer.com/search?q={query}")
+    headers = {
+        "Authorization": f"Bearer {DEEZER_ACCESS_TOKEN}"
+    }
+    response = requests.get(f"https://api.deezer.com/search?q={query}", headers=headers)
 
     if response.status_code == 200:
         data = response.json()
@@ -18,7 +22,7 @@ def download_music(_, message):
             track = data["data"][0]  # İlk müziği seçiyoruz
             title = track["title"]
             artist = track["artist"]["name"]
-            audio_url = track["preview"]
+            audio_url = track["link"]
 
             # Müziği indiriyoruz
             r = requests.get(audio_url, stream=True)
@@ -28,9 +32,7 @@ def download_music(_, message):
                         f.write(chunk)
 
                 # İndirme başarılı olduğunda kullanıcıya müziği gönderiyoruz
-                # Değiştirdiğimiz kısım: Tam parçayı göndermek için URL'i değiştiriyoruz
-                audio_url = track["link"]
-                message.reply_audio(audio_url, caption=f"{title} - {artist}")
+                message.reply_audio(f"{title}.mp3", caption=f"{title} - {artist}")
 
                 # İndirilen müziği silmek için aşağıdaki satırı ekleyebilirsiniz
                 # os.remove(f"{title}.mp3")
