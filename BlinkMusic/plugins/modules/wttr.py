8cd3cc3355236186579566d534d5639636596c55
heda <103752316+hedala@users.noreply.github.com>
2023-07-08 08:49:55 +0300

    Update wttr.py

--- BlinkMusic/plugins/modules/wttr.py
+++ BlinkMusic/plugins/modules/wttr.py
@@ -1,25 +1,22 @@
-import requests
 from BlinkMusic import app
 from pyrogram import filters
+import requests
 
-
-def get_weather(city):
-    url = f"https://wttr.in/{city.replace(' ', '+')}?qTc"
+def get_weather_info(city):
+    url = f"https://wttr.in/{city}?qT0"
     response = requests.get(url)
     if response.status_code == 200:
         return response.text
     return "Hava durumu bilgisi alınamadı."
 
-
 @app.on_message(filters.command("hv"))
 def weather(_, message):
     if len(message.command) > 1:
         city = " ".join(message.command[1:])
-        weather_info = get_weather(city)
+        weather_info = get_weather_info(city)
         message.reply_text(weather_info)
     else:
         message.reply_text("Lütfen bir şehir adı belirtin.")
-
 
 if __name__ == "__main__":
     app.run()
