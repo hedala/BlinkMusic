00ab07d412755f3bc08cb221ec1663d20e72b304
heda <103752316+hedala@users.noreply.github.com>
2023-07-08 09:36:12 +0300

    Update wttr.py

--- BlinkMusic/plugins/modules/wttr.py
+++ BlinkMusic/plugins/modules/wttr.py
@@ -1,16 +1,12 @@
-from datetime import datetime
-from pytz import timezone
 from BlinkMusic import app
 from pyrogram import filters
 import requests
 
 def get_weather_info(city):
-    url = f"https://wttr.in/{city}?qT0m"
+    url = f"https://wttr.in/{city}?qmT0"
     response = requests.get(url)
     if response.status_code == 200:
-        weather_info = response.text.strip()
-        current_time = datetime.now(timezone('Europe/Istanbul')).strftime('%Y-%m-%d %H:%M:%S')
-        return f"Hava Durumu (Son Güncelleme Zamanı: {current_time}):\n\n{weather_info}"
+        return response.text
     return "Hava durumu bilgisi alınamadı."
 
 @app.on_message(filters.command("hv"))
