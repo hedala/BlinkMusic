de2a740d97b1511c6591e0845ae61fa5c36e8647
heda <103752316+hedala@users.noreply.github.com>
2023-07-10 13:11:45 +0300

    Update pic.py

--- BlinkMusic/plugins/modules/pic.py
+++ BlinkMusic/plugins/modules/pic.py
@@ -2,16 +2,17 @@ from pyrogram import Client, filters
 from BlinkMusic import app
 
 def get_profile_photo(app, user_id):
-    photos = app.get_profile_photos(user_id, limit=1)
-    if photos.total_count > 0:
-        photo = photos[0]
+    # A list of ProfilePhoto
+    photos = app.get_user_profile_photos(user_id, limit=1)
+    if len(photos.photos) > 0:
+        photo = photos.photos[0]
         photo_path = app.download_media(photo.file_id)
         return photo_path
     else:
         return None
 
 def get_all_profile_photos(app, user_id):
-    photos = app.get_profile_photos(user_id, limit=1000)
+    photos = app.get_user_profile_photos(user_id, limit=10)
     photo_paths = []
     for photo in photos.photos:
         photo_path = app.download_media(photo.file_id)
@@ -22,7 +23,7 @@ def get_all_profile_photos(app, user_id):
 def pic(_, message):
     user_id = message.from_user.id
     photo_path = get_profile_photo(app, user_id)
-    if photo_path != None:
+    if photo_path is not None:
         with open(photo_path, 'rb') as photo:
             message.reply_photo(photo)
     else:
@@ -33,7 +34,7 @@ def picall(_, message):
     user_id = message.from_user.id
     photo_paths = get_all_profile_photos(app, user_id)
     for photo_path in photo_paths:
-        if photo_path != None:
+        if photo_path is not None:
             with open(photo_path, 'rb') as photo:
                 message.reply_photo(photo)
         else:
